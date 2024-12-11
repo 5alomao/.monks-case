9a2cc04b68a709a84922551423e99e651f8dd14c
5alomao <salomao.junior2k22@gmail.com>
2024-12-11 13:06:11 -0300

    Adicionando animação após carregar os dados do arquivo JSON

--- js/loadProductTags.js
+++ js/loadProductTags.js
@@ -1,31 +1,58 @@
 async function fetchProductTagsData() {
-    try {
-        const response = await fetch("./data/product-tags.json");
-        if (!response.ok) {
-            throw new Error("Network response was not ok");
-        }
-        const productTagsData = await response.json();
-        populateProductTags(productTagsData);
-
-    } catch (error) {
-        console.error("Error fetching the product tags data:", error);
+  try {
+    const response = await fetch("./data/product-tags.json");
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
     }
+    const productTagsData = await response.json();
+    populateProductTags(productTagsData);
+  } catch (error) {
+    console.error("Error fetching the product tags data:", error);
+  }
 }
 
 function populateProductTags(productTagsData) {
-    const productTagsList = document.getElementById("tags-container");
-    productTagsList.innerHTML = "";
-    productTagsData.forEach((tag) => {
-        productTagsList.innerHTML += createProductTagCard(tag);
-    });
+  const productTagsList = document.getElementById("tags-container");
+  productTagsList.innerHTML = "";
+  productTagsData.forEach((tag) => {
+    productTagsList.innerHTML += createProductTagCard(tag);
+  });
+
+  initTagObservers();
 }
 
 function createProductTagCard(tag) {
-    return `
+  return `
         <div class="product-tag">
             <p>${tag.name}</p>
         </div>
     `;
+}
+
+function initTagObservers() {
+  const tagItems = document.querySelectorAll("#tags-container > .product-tag");
+  console.log(tagItems.length);
+
+  const observerCallback = (entries, observer) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("visible");
+        observer.unobserve(entry.target);
+      }
+    });
+  };
+
+  const observerOptions = {
+    threshold: 0.2,
+  };
+
+  const observer = new IntersectionObserver(observerCallback, observerOptions);
+
+  tagItems.forEach((tag) => {
+    setInterval(() => {
+      observer.observe(tag);
+    }, 1000);
+  });
 }
 
 document.addEventListener("DOMContentLoaded", fetchProductTagsData);
