d4087be770772d8785ab88457a2193ecff2972d7
5alomao <salomao.junior2k22@gmail.com>
2024-12-11 13:07:12 -0300

    Adicionando window resize para desativar o menu burguer em telas maiores

--- js/nav-menu.js
+++ js/nav-menu.js
@@ -4,16 +4,28 @@ const navMenu = document.querySelector(".nav-menu");
 const navOptions = document.getElementById("nav-options");
 const navList = document.querySelector(".nav-list");
 
-// Alternar menu ao clicar
 menuBars.addEventListener("click", () => {
   nav.classList.toggle("active");
   navMenu.classList.toggle("active");
   navOptions.classList.toggle("active");
   navList.classList.toggle("active");
 
-  // Mudar cor dos links e do ::before quando o menu estiver ativo
   const navItems = document.querySelectorAll(".nav-list-item a");
   navItems.forEach((item) => {
-    item.classList.toggle("active"); // Aplica a classe 'active' ao link
+    item.classList.toggle("active");
   });
+});
+
+window.addEventListener("resize", () => {
+  if (window.innerWidth > 840) {
+    nav.classList.remove("active");
+    navMenu.classList.remove("active");
+    navOptions.classList.remove("active");
+    navList.classList.remove("active");
+
+    const navItems = document.querySelectorAll(".nav-list-item a");
+    navItems.forEach((item) => {
+      item.classList.remove("active");
+    });
+  }
 });
